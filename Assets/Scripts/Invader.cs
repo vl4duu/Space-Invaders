eb7e1d3b0da28e75a9ac4c590374c43c02042784
vl4duu <vlad.mutilica@gmail.com>
2022-02-13 18:49:58 +0000

    implemented invaders and their movement

---
+++ Assets/Scripts/Invader.cs
@@ -0,0 +1,28 @@
+
+using UnityEngine;
+
+public class Invader : MonoBehaviour
+{
+    public Sprite[] animationSprites;
+    public float animationTime;
+    private SpriteRenderer _spriteRenderer;
+    private int _animationFrame;
+
+    private void Awake(){
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start(){
+        InvokeRepeating(nameof(AnimateSprite), this.animationTime, this.animationTime);
+    }
+
+    private void AnimateSprite(){
+        _animationFrame++;
+
+        if (_animationFrame >= this.animationSprites.Length){
+            _animationFrame = 0;
+        }
+
+        _spriteRenderer.sprite = this.animationSprites[_animationFrame];
+    }
+}
---
+++ Assets/Scripts/Invaders.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Invaders : MonoBehaviour
+{
+    public Invader[] prefabs;
+    public int rows = 5 ;
+    public int columns = 11;
+    public float speed = 5.0f;
+    private Vector3 _direction = Vector2.right;
+    void Awake()
+    {
+        for (int row = 0; row < this.rows; row++){
+            float width = 2.0f * (this.columns -1);
+            float height = 2.0f * (this.rows -1);
+            Vector2 centering = new Vector2(-width /2, -height /2) ;
+            Vector3 rowPosition = new Vector3(centering.x, centering.y + (row * 3.0f) ,0.0f);
+
+            for(int column = 0; column < this.columns; column++){
+                Invader invader = Instantiate(this.prefabs[row], this.transform);
+                Vector3 position = rowPosition;
+                position.x += column * 2.0f;
+                invader.transform.localPosition = position;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        this.transform.position += _direction * this.speed * Time.deltaTime;
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+        foreach(Transform invader in this.transform)
+        {
+            if(!invader.gameObject.activeInHierarchy){
+                continue;
+            }
+            if(_direction == Vector3.right && invader.position.x >= (rightEdge.x - 1.0f)){
+                AdvanceRow();
+            }else if(_direction == Vector3.left && invader.position.x <= (leftEdge.x + 1.0f)){
+                AdvanceRow();
+            }
+        }
+    }
+
+    private void AdvanceRow(){
+        _direction.x *= -1.0f;
+
+        Vector3 position = this.transform.position;
+        this.transform.position = position;
+    }
+}
