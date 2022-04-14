7de8cab4361e73c43690c7a14ddb21cc035202b3
Vl4duu <vlad.mutilica@gmail.com>
2022-04-14 06:36:05 +0100

    Implemented writing, working on newLine

--- Assets/Animations/Main menu console/Console.cs
+++ Assets/Animations/Main menu console/Console.cs
@@ -9,19 +9,31 @@ using UnityEngine.UIElements;
 
 public class Console : MonoBehaviour
 {
+    private System.Action delete;
+    private System.Action newLine;
     private InputField input;
     private char character;
-    private bool backspace;
+    private string tempString;
+    public bool KeyDownEvent { get; set; }
+    private bool tempBool;
 
     private void Awake()
     {
         input = GetComponent<InputField>();
+        delete += DeleteChar;
+        newLine += NewLine;
     }
 
-
-
-    private void Update()
+    private void DeleteChar()
     {
+        input.text = input.text.Remove(input.text.Length - 1, 1);
+    }
+    
+
+    void FixedUpdate()
+    {
+        input.text += character;
+        character = '\0';
         try
         {
             OnGUI();
@@ -31,31 +43,53 @@ public class Console : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
-    {
-        input.text += character;
-        character = '\0';
-        if (backspace)
-        {
-            string temp = input.text.Remove(input.text.Length - 1, 1);
-            input.text = temp;
-            backspace = false;
-            Debug.Log(input.text.Remove(input.text.Length - 1, 1));
-        }
-    }
-
     void OnGUI()
     {
         Event e = Event.current;
         if (e.isKey)
         {
-            if (e.keyCode == KeyCode.Backspace)
+            if (e.keyCode == KeyCode.LeftShift || e.keyCode == KeyCode.LeftShift)
             {
-                backspace = true;
+                e.keyCode = KeyCode.None;
             }
-            character = (char) e.keyCode;
+            else if (e.keyCode == KeyCode.Backspace)
+            {
+                try
+                {
+                    delete.Invoke();
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                }
+
+                e.keyCode = KeyCode.None;
+            }
+            else if (e.keyCode == KeyCode.Return)
+            {
+                this.newLine.Invoke();
+                tempBool = true;
+                e.keyCode = KeyCode.None;
+            }
+            else
+            {
+                character = (char) e.keyCode;
+            }
         }
     }
 
-    public bool KeyDownEvent { get; set; }
+    void NewLine()
+    {
+        if(tempBool)
+        {
+            Debug.Log("Enter");
+            tempString = input.text;
+            GameObject parent = this.transform.parent.gameObject;
+            float y = parent.transform.position.y - parent.transform.localScale.y;
+            GameObject temp = Instantiate(parent, parent.transform.parent);
+            tempBool = false;
+        }
+        
+    }
+
+
 }
