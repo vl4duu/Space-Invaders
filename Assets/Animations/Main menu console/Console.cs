ffdee812099f7353024378d7410e124c685c92bc
Vl4duu <vlad.mutilica@gmail.com>
2022-04-14 04:15:03 +0100

    implemented writing, working on deleting

--- Assets/Animations/Main menu console/Console.cs
+++ Assets/Animations/Main menu console/Console.cs
@@ -1,23 +1,62 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Console : MonoBehaviour
 {
-    private GameObject input;
+    private InputField input;
+    private char character;
+    private bool backspace;
 
     private void Awake()
     {
-        input = GameObject.Find("Input");
+        input = GetComponent<InputField>();
     }
 
-    private void Start()
+
+
+    private void Update()
     {
-        if (input.GetComponent<InputField>().text == "Hello")
+        try
         {
-            Debug.Log("Hello! :D");
+            OnGUI();
+        }
+        catch (NullReferenceException)
+        {
         }
     }
+
+    void FixedUpdate()
+    {
+        input.text += character;
+        character = '\0';
+
+        if (backspace)
+        {
+            string temp = input.text.Remove(input.text.Length - 1, 1);
+            input.text = temp;
+            backspace = false;
+            Debug.Log(input.text.Remove(input.text.Length - 1, 1));
+        }
+    }
+
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey)
+        {
+            if (e.keyCode == KeyCode.Backspace)
+            {
+                backspace = true;
+            }
+            character = (char) e.keyCode;
+        }
+    }
+
+    public bool KeyDownEvent { get; set; }
 }
